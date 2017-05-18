8aef9fa7990eec4c3cccc49ac603b6e68e666faf
Rainydayfmb <896720452@qq.com>
2017-05-18 23:18:09 +0800

    单元测试

---
+++ src/main/java/cn/springmvc/dao/DeptMapper.java
@@ -0,0 +1,17 @@
+package cn.springmvc.dao;
+
+import cn.springmvc.model.Dept;
+
+public interface DeptMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Dept record);
+
+    int insertSelective(Dept record);
+
+    Dept selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Dept record);
+
+    int updateByPrimaryKey(Dept record);
+}
---
+++ src/main/java/cn/springmvc/dao/DocumentMapper.java
@@ -0,0 +1,17 @@
+package cn.springmvc.dao;
+
+import cn.springmvc.model.Document;
+
+public interface DocumentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Document record);
+
+    int insertSelective(Document record);
+
+    Document selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Document record);
+
+    int updateByPrimaryKey(Document record);
+}
