d7d0d22b933cce14c2499728f22ac825075a69b7
feipeng <896720452@qq.com>
2017-05-18 17:44:29 +0800

    添加单元测试成功

--- src/test/java/cn/springmvc/test/UserTest.java
+++ src/test/java/cn/springmvc/test/UserTest.java
@@ -4,24 +4,33 @@ import java.awt.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cn.springmvc.model.User;
 import cn.springmvc.service.UserService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:conf/spring.xml","classpath:conf/spring-mybatis.xml"})
 public class UserTest {
+
+    @Resource
 	private UserService userService;
 
 
-	@Before
+	/*@Before
     public void before(){
 
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:conf/spring.xml"
                 ,"classpath:conf/spring-mybatis.xml"});
         userService = (UserService) context.getBean("userServiceImpl");
-    }
+    }*/
 
     @Test
     public void addUser(){
