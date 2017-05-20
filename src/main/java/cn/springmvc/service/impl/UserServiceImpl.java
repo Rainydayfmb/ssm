2d7c590ae68963217312fd6fafbfb872414f794a
Rainydayfmb <896720452@qq.com>
2017-05-21 01:37:34 +0800

    增加登陆功能和功能页面

--- src/main/java/cn/springmvc/service/impl/UserServiceImpl.java
+++ src/main/java/cn/springmvc/service/impl/UserServiceImpl.java
@@ -1,24 +1,26 @@
 package cn.springmvc.service.impl;
 
-import java.awt.List;
-
+import cn.springmvc.dao.UserMapper;
+import cn.springmvc.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cn.springmvc.dao.UserDAO;
-import cn.springmvc.model.User;
 import cn.springmvc.service.UserService;
 
 
 @Service
 public class UserServiceImpl implements  UserService{
 	@Autowired
-    private UserDAO userDAO;
+    private UserMapper userMapper;
      
     public int insertUser(User user) {
         // TODO Auto-generated method stub
-        return userDAO.insertUser(user);
+       return 0;
     }
- 
+
+    public User login(String loginname, String password) {
+        return  userMapper.selectByLoginnameAndPassword(loginname, password);
+    }
+
 }
  
